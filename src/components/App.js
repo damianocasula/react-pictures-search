fce582fa63d4aae014d95dbeaa9ce91874c59bef
Damiano <95046135+damianoc-linear@users.noreply.github.com>
2022-04-26 21:59:17 +0200

    feat: make request to Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
   onSearchSubmit (term) {
-    console.log(`Searching for ${term}`)
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization:
+            'Client-ID ' + process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+        }
+      })
+      .then(response => {
+        console.log(response)
+      })
   }
 
   render () {
