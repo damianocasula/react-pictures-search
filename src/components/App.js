8336e56504759792a50cc668d734aed10a9a9ef2
Damiano <95046135+damianoc-linear@users.noreply.github.com>
2022-04-28 21:29:16 +0200

    feat: show number of images found

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,26 @@ import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-  onSearchSubmit (term) {
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: {
-          query: term
-        },
-        headers: {
-          Authorization:
-            'Client-ID ' + process.env.REACT_APP_UNSPLASH_ACCESS_KEY
-        }
-      })
-      .then(response => {
-        console.log(response)
-      })
+  state = { images: [] }
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID ' + process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+      }
+    })
+
+    this.setState({ images: response.data.results })
   }
 
   render () {
     return (
       <div className='ui container' style={{ marginTop: '1rem' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
