92092a6e93e661bd577e6c09d1406aba96287528
Damiano <95046135+damianoc-linear@users.noreply.github.com>
2022-04-30 17:37:41 +0200

    feat: use unsplash api controller

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import axios from 'axios'
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
   state = { images: [] }
 
   onSearchSubmit = async term => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+    const response = await unsplash.get('/search/photos', {
       params: {
         query: term
-      },
-      headers: {
-        Authorization: 'Client-ID ' + process.env.REACT_APP_UNSPLASH_ACCESS_KEY
       }
     })
 
